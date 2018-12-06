8f9330a6c943bdde0e3286469d5533a2c66f8604
Veera Itälinna <veera.italinna@aalto.fi>
2018-12-06 11:54:53 +0200

    added comments

--- day03/no_matter_how_you_slice_it.py
+++ day03/no_matter_how_you_slice_it.py
@@ -27,6 +27,8 @@ fabric = np.zeros((1000, 1000))
 for claim in claims:
     s1 = slice(claim['left'], claim['left']+claim['width'])
     s2 = slice(claim['top'], claim['top']+claim['height'])
+    # if there is already a claim for some square inches, update value to -1
+    # otherwise mark the square inches with claim ID
     fabric[s1, s2][np.where(fabric[s1, s2] > 0)] = -1
     fabric[s1, s2][np.where(fabric[s1, s2] == 0)] = claim['id']
 print(len(np.where(fabric < 0)[0]), "overlapping square inches")
@@ -34,10 +36,12 @@ print(len(np.where(fabric < 0)[0]), "overlapping square inches")
 
 ### PART TWO ###
 
+# use the filled matrix from part one
 intact_id = ''
 for claim in claims:
-    intact_indices = np.where(fabric == claim['id'])
     intact_size = np.size(fabric[fabric == claim['id']])
+    # if the number of non-overlapping matrix elements is the same as the size of the rectangle,
+    # there is no overlapping at all
     if intact_size == claim['height'] * claim['width']:
         intact_id = claim['id']
         break
